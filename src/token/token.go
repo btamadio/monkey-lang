94420d41ec23898dabd9a1107059ea496d959d28
Brian Amadio <brian.amadio@stitchfix.com>
2020-10-18 08:42:19 -0700

    first commit

---
+++ src/token/token.go
@@ -0,0 +1,65 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+// Token types
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// identifiers and literals
+	IDENT = "IDENT"
+	INT = "INT"
+
+	// operators
+	ASSIGN = "="
+	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+	LT = "<"
+	GT = ">"
+	EQ = "=="
+	NOT_EQ = "!="
+
+	// special characters
+	COMMA = ","
+	SEMICOLON = ";"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+)
+
+// language keywords are defined here
+var keywords = map[string]TokenType {
+	"fn": FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT // if it's not a keyword, then it's a user-defined identifier
+}
