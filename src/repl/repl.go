1c22af3856778e82fd50a5c85023e92e600192a6
Brian Amadio <brian.amadio@stitchfix.com>
2020-10-18 15:10:48 -0700

    evaluator and objects

---
+++ src/repl/repl.go
@@ -0,0 +1,46 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/btamadio/monkey_lang/src/evaluator"
+	"github.com/btamadio/monkey_lang/src/lexer"
+	"github.com/btamadio/monkey_lang/src/parser"
+	"io"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		_, _ = fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) > 0 {
+			printParserErrors(out, p.Errors())
+			continue
+		}
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			_, _ = io.WriteString(out, evaluated.Inspect())
+			_, _ = io.WriteString(out, "\n")
+		}
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		_, _ = io.WriteString(out, "\t" + msg + "\t")
+	}
+}
